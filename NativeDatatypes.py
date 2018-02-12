184d85095146d9ab4b4f1d9e623953fc24c9b8e7
Pavel Pavlov <codedriverun@gmail.com>
2018-02-12 18:32:32 +0200

    Create NativeDatatypes.py
    Examples of the most used datatypes in python

---
+++ NativeDatatypes.py
@@ -0,0 +1,10 @@
+Booleans (True or False)
+Numbers can be integers(1 and 2), floats(1.1 and 1.2), fractions( 1/2 and 2/3) or complex numbers
+Strings are sequences of Unicode characters, e.g. an HTML document
+Bytes and byte arrays e.g. a Jpeg image file
+Lists are ordered sequences of values
+Tuples are ordered immutable sequences of values
+Sets are unordered bags of values
+Dictionaries are unordered bags of key-value pairs
+
+There  are more types of objects like module,function,method,file and compiled code
