cdef60754d51c33a29c3df9fe06761a17e94ec7d
Pavlov83 <codedriverun@gmail.com>
2018-02-17 20:53:54 +0200

    lists

--- Dive_into_python/Lists.py
+++ Dive_into_python/Lists.py
@@ -2,3 +2,10 @@
 can expand dynamically as new items are added.'''
 
 
+# Creating  a list
+a_list = ['myObject','Git',123,'secondObject']
+print(a_list)
+
+# As expected,lists have zero based indexes so you can print particular indexes
+
+a_list[2]
