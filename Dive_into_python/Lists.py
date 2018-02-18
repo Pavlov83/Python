8fed19f049d4a2407e6a1b7a4a80a7c8abb4d2fe
Pavlov83 <codedriverun@gmail.com>
2018-02-18 21:09:44 +0200

    examples of adding obejcts to lists and manipulating strings

--- Dive_into_python/Lists.py
+++ Dive_into_python/Lists.py
@@ -3,9 +3,30 @@ can expand dynamically as new items are added.'''
 
 
 # Creating  a list
-a_list = ['myObject','Git',123,'secondObject']
+a_list = ['myObject','Git',123,'secondObject','string',True,'another string']
 print(a_list)
 
 # As expected,lists have zero based indexes so you can print particular indexes
 
-a_list[2]
+print(a_list[2])
+
+''' Slicing is used to get any needed part of the list.To get any part of the
+list,we need to specify two indices- beginning and end of the slice.
+'''
+
+print (a_list[0:2])
+
+# We also can concatenate lists and the result is a new list
+list = [123,'string']+['second value']
+print (list)
+
+
+'''if you want to add item at the end of the list you need to use the append()
+function '''
+a_list.append(3)
+print(a_list)
+
+''' another way of adding elements to a list is the extend() function which
+accepts list as argument '''
+a_list.extend(['additional element','another element'])
+print(a_list)
