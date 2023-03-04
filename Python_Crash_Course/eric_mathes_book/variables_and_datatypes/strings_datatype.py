c75e14613d3923d9b7d21d49c9483331d66ffea0
Pavlov83 <resolversoft@gmail.com>
2023-03-04 13:37:40 +0200

    Python crash course added variables and strings

---
+++ Python_Crash_Course/eric_mathes_book/variables_and_datatypes/strings_datatype.py
@@ -0,0 +1,10 @@
+name = "walter white"
+
+#title() is built-in method that adds capital latters to new words
+print(name.title())
+
+#We can lowercase all words
+print(name.lower())
+
+#We can uppercase letters
+print(name.upper())
---
+++ Python_Crash_Course/eric_mathes_book/variables_and_datatypes/variable_declaration.py
@@ -0,0 +1,24 @@
+print("Hello World Python")
+
+# we can store our message in a variable and give it as argument
+
+message = "Welcome to the world of Python"
+
+print(message)
+
+'''
+Variable names can contain only letters,numbers and underscores
+They can start with letter or underscore but not with number
+
+Spaces are not allowed in variable names
+
+Keywords are not allowed in variable names
+
+'''
+
+#The variable is reference or label to some data
+#The data can be changed
+
+message = "This is the new message"
+
+print(message)
