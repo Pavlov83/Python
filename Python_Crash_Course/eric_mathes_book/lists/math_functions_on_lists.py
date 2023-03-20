e674d85938415004fa5d96ed2d49a8ab1b9d2133
Pavlov83 <resolversoft@gmail.com>
2023-03-20 20:47:10 +0200

    list math methods

---
+++ Python_Crash_Course/eric_mathes_book/lists/math_functions_on_lists.py
@@ -0,0 +1,9 @@
+digits = []
+for i in range(1,10):
+    digits.append(i*i)
+
+print("smallest number is: ", min(digits))
+
+print("highest number is: ", max(digits))
+
+print("The sum of all numbers is: ", sum(digits))
