f7abec293b34135e548cc2a32097afe51286889d
Pavlov83 <resolversoft@gmail.com>
2023-04-14 10:16:02 +0300

    Binary search extended

---
+++ AlgorithmsAndDataStructures with Projects/Python Basics/array.py
@@ -0,0 +1,17 @@
+import array
+
+arr = array.array('i',[1,2,3])
+
+#printing original array
+print("The newly created array is :", end=" ")
+for i in rahge(0,3):
+    print(arr[i], end=" ")
+
+print("\r")
+
+#Using append() to insert new value to the end
+arr.append(4);
+
+print(T="The appended array is :")
+for i in range(0,4):
+    print(arr[i], end=" ")
