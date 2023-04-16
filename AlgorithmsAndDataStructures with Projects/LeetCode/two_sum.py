428bee17a93ba3ddb9440f470dec8b0760d29bee
Pavlov83 <resolversoft@gmail.com>
2023-04-16 20:36:07 +0300

    Leetcode two sum problem

--- AlgorithmsAndDataStructures with Projects/LeetCode/two_sum.py
+++ AlgorithmsAndDataStructures with Projects/LeetCode/two_sum.py
@@ -1,11 +1,12 @@
-def two_sum(list,target):
-    seen = {}
-    for i in range(len(list)):
-        diff = target - list[i]
-        if diff in seen:
-            return [seen[diff], i]
-        else:
-            seen[list[i]] = i
+class Solution():
+    def two_sum(list,target):
+        seen = {}
+        for i in range(len(list)):
+            diff = target - list[i]
+            if diff in seen:
+                return [seen[diff], i]
+            else:
+                seen[list[i]] = i
 
 myList = [1,2,3,4,5,6,7,8,9]
 
